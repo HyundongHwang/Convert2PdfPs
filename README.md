ccd88a06ea3a2521c19be4a64d7ba1c90454c049
HyundongHwang <hhd2002@gmail.com>
2018-02-23 15:33:28 +0900

    modify

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# ConvertPdfPs
-ConvertPdfPs
+# convert2pdf
+
+<!-- TOC -->
+
+- [convert2pdf](#convert2pdf)
+- [종속성](#종속성)
+- [설치](#설치)
+- [사용](#사용)
+
+<!-- /TOC -->
+
+워드파일, 파워포인트파일을 pdf로 컨버팅해주는 스크립트 입니다. <br>
+`ls -Recurse` 와 연결해서 구석구석 모든 파일을 pdf로 변환하면 편합니다. <br>
+pdf문서로 변환해서 태블릿, 전자책리더에서 편하게 문서를 봅시다. ^^ <br>
+
+<br>
+<br>
+<br>
+
+# 종속성
+- MS Word
+- MS PowerPoint
+
+<br>
+<br>
+<br>
+
+# 설치
+
+```powershell
+PS> Install-Module -Name convert2pdf
+```
+
+<br>
+<br>
+<br>
+
+
+# 사용
+
+```powershell
+PS> ls -Recurse | convert2pdf
+
+C:\temp\doc\aaa.pptx start ...
+C:\temp\doc\aaa.pdf done ...
+0
+C:\temp\doc\aaa.pdf
+C:\temp\doc\bbb.docx start ...
+C:\temp\doc\bbb.pdf done ...
+0
+C:\temp\doc\bbb.pdf
+C:\temp\doc\ccc.docx start ...
+C:\temp\doc\ccc.pdf done ...
+0
+C:\temp\doc\ccc.pdf
+...
+```
+<br>
+<br>
+<br>
---
+++ convert2pdf.psm1
@@ -0,0 +1,55 @@
+﻿# $global:G_STORAGE_CONTEXT = $null
+
+
+
+<#
+.SYNOPSIS
+.EXAMPLE
+#>
+function convert2pdf
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelinebyPropertyName=$true)]
+        [System.IO.FileInfo]
+        $FILE_PATH
+    )
+
+    process {
+        $pdfPath = "$($FILE_PATH.DirectoryName)\$($FILE_PATH.BaseName).pdf"
+
+        if (Test-Path $pdfPath) {
+            Write-Host "$pdfPath already exist !!!"
+            Write-Output $pdfPath
+            return
+        }
+
+
+
+        if(($FILE_PATH -like "*.doc") -or ($FILE_PATH -like "*.docx")) {
+            $wordCom = New-Object -ComObject Word.Application
+            $doc = $wordCom.Documents.Open($FILE_PATH.FullName)
+            Write-Host "$($FILE_PATH.FullName) start ..."
+            $doc.SaveAs($pdfPath, 17)
+            $doc.Close()
+            Write-Host "$pdfPath done ..."
+            $wordCom.Quit()
+            [System.Runtime.Interopservices.Marshal]::ReleaseComObject($wordCom)
+            Write-Output $pdfPath
+        }
+
+        if(($FILE_PATH -like "*.ppt") -or ($FILE_PATH -like "*.pptx")) {
+            $pptCom = New-Object -ComObject PowerPoint.Application
+            $doc = $pptCom.Presentations.Open($FILE_PATH.FullName)
+            Write-Host "$($FILE_PATH.FullName) start ..."
+            $opt= [Microsoft.Office.Interop.PowerPoint.PpSaveAsFileType]::ppSaveAsPDF
+            $doc.SaveAs($pdfPath, $opt)
+            $doc.Close()
+            Write-Host "$pdfPath done ..."
+            $pptCom.Quit()
+            [System.Runtime.Interopservices.Marshal]::ReleaseComObject($pptCom)
+            Write-Output $pdfPath
+        }
+    }
+}
